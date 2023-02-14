d5e4931a428cd3dc8d970933b463983118d51aac
freenbeckyz <1370321929@qq.com>
2023-02-14 16:41:07 +0800

    第一次

--- demo-module-1/src/com/zh/MainTest.java
+++ demo-module-1/src/com/zh/MainTest.java
@@ -4,6 +4,6 @@ public class MainTest {
     public static void main(String[] args)
     {
         System.out.println("k发");
-        System.out.println("kjhkll");
+        System.out.println("kjhklllh");
     }
 }
