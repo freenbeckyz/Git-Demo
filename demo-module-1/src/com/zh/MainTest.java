60b76609381d9d304a87232594d515de5e01aa98
freenbeckyz <1370321929@qq.com>
2023-02-14 16:10:52 +0800

    尝试第二次更改

--- demo-module-1/src/com/zh/MainTest.java
+++ demo-module-1/src/com/zh/MainTest.java
@@ -3,7 +3,7 @@ package com.zh;
 public class MainTest {
     public static void main(String[] args)
     {
-        System.out.println("");
+        System.out.println("k");
         System.out.println("kjhkll");
     }
 }
